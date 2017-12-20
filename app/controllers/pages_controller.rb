3aeed8515aa58e5c576b52ce4d57cd9b0caf2abc
Antonio Fearon <antonio.l.fearon@gmail.com>
2017-12-19 20:49:45 -0500

    Bug which caused incorrect campaign selections addressed.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,13 +17,16 @@ class PagesController < ApplicationController
     if session[:current_campaign_id]
       # @campaign = session[:current_campaign_id]
       @campaign = Campaign.find(session[:current_campaign_id])
-      Rails.logger.debug("Camp Try")
-      Rails.logger.debug(@campaign)
+      Rails.logger.debug("========")
+      Rails.logger.debug("Campaign Home")
+      Rails.logger.debug(@campaign.title)
       Rails.logger.debug("========")
       # Rails.logger.debug(@campaign)
       # session.delete(:current_campaign)
     else
+      Rails.logger.debug("====ELSE====")
       @campaign = Campaign.current
+      Rails.logger.debug("====ELSE====")
     end
     # @campaign = Campaign.current
   end
@@ -33,13 +36,16 @@ class PagesController < ApplicationController
   end
 
   def constituency_campaign
-    # Rails.logger.debug("Camp Try")
-    # Rails.logger.debug(params.inspect)
-    # Rails.logger.debug(params[:id])
+    Rails.logger.debug("=========================================")
+    Rails.logger.debug("Constituency Camp Try")
+    Rails.logger.debug(params.inspect)
+    Rails.logger.debug(params[:id])
     @constituency = Constituency.find(params[:id])
-    # Rails.logger.debug(@constituency.title)
+    Rails.logger.debug(@constituency.title)
     @campaign = @constituency.current_campaign
-    # Rails.logger.debug(@campaign.title)
+    Rails.logger.debug(@campaign.title)
+    Rails.logger.debug("=========================================")
+
     session[:current_campaign_id] = @campaign.id
     # Rails.logger.debug(session[:current_campaign].title)
     # session.delete(:current_campaign)
--- app/controllers/voting/proposals_controller.rb
+++ app/controllers/voting/proposals_controller.rb
@@ -3,12 +3,18 @@ class Voting::ProposalsController < ApplicationController
     if session[:verification_pending]
       flash.now[:notice] = _('<strong>Verification pending</strong>, please see your inbox for further instructions.')
     end
-    @campaign_budget = Campaign.current.budget
+    # @campaign_budget = Campaign.current.budget
+    @campaign = Campaign.find(session[:current_campaign_id])
+
+    @campaign_budget = @campaign.budget
 
     classifiers_filter = params[:class].split(',').map(&:to_i) if params[:class]
     budget_min_filter = params[:budget_min]
     budget_max_filter = params[:budget_max]
-    @proposals = Campaign.current.proposals.includes(:classifiers)
+    # @proposals = Campaign.current.proposals.includes(:classifiers)
+
+    @proposals = @campaign.proposals.includes(:classifiers)
+
     @budget_max = @proposals.max_by(&:budget)&.budget&.ceil || 0
 
     Rails.logger.debug("===========")
@@ -44,14 +50,14 @@ class Voting::ProposalsController < ApplicationController
     bad_proposals ||= []
     count = 1
     @proposals.each do |prop|
-      printf "count: "
-      Rails.logger.debug(count)
-      Rails.logger.debug("<===========>")
-      Rails.logger.debug(prop.title)
+      # printf "count: "
+      # Rails.logger.debug(count)
+      # Rails.logger.debug("<===========>")
+      # Rails.logger.debug(prop.title)
       # Rails.logger.debug(prop.tags.classifier_id)
       if classifiers_filter.present?
-        Rails.logger.debug(classifiers_filter)
-        Rails.logger.debug(prop.classifier_ids)
+        # Rails.logger.debug(classifiers_filter)
+        # Rails.logger.debug(prop.classifier_ids)
         # @bool = classifiers_filter == prop.classifier_ids ? "TRUE" : "FALSE"
         # Rails.logger.debug(@bool)
         @bool2 = (prop&.classifier_ids & classifiers_filter) == prop&.classifier_ids ? "TRUE" : "FALSE" #prop&.classifier_ids #(prop.classifier_ids - classifiers_filter).empty?
@@ -59,23 +65,23 @@ class Voting::ProposalsController < ApplicationController
         if !((prop&.classifier_ids & classifiers_filter) == prop&.classifier_ids)
           # @proposals.delete(prop)
           bad_proposals.push(prop)
-          Rails.logger.debug("Does Not belong!")
+          # Rails.logger.debug("Does Not belong!")
         end
-        Rails.logger.debug(@bool2)
+        # Rails.logger.debug(@bool2)
 
         # Rails.logger.debug(prop&.classifiers)
-        Rails.logger.debug("<===========>")
+        # Rails.logger.debug("<===========>")
       end
       count += 1
     end
-    printf "bad size: "
-    Rails.logger.debug(bad_proposals.size)
+    # printf "bad size: "
+    # Rails.logger.debug(bad_proposals.size)
 
     # cleaned_proposals = @proposals - bad_proposals
     @proposals = @proposals - bad_proposals
 
-    printf "Cleaned size: "
-    Rails.logger.debug(@proposals.size)
+    # printf "Cleaned size: "
+    # Rails.logger.debug(@proposals.size)
 
     @districts = classifiers.select{ |_| _.is_a?(District) }
     @areas = classifiers.select { |_| _.is_a?(Area) }
